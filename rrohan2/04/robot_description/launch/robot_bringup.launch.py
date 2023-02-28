e5372952700bdc250accc2315d33c0499192b794
ritwik <rrihan2@jh.edu>
2023-02-28 17:06:22 -0600

    assignment4

---
+++ rrohan2/04/robot_description/launch/robot_bringup.launch.py
@@ -0,0 +1,83 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, TimerAction
+from launch.event_handlers import OnProcessStart
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="robot_description",
+            description="Robot description",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "name",
+            default_value='"arm"',
+            description="Joint name prefix.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "parent",
+            default_value='"world"',
+            description="Parent fixed frame",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "namespace",
+            default_value="arm",
+            description="Namespace of robot_state_ publisher and joint_state_publisher",
+        )
+    )
+
+    # Initialize Arguments
+    description_package = LaunchConfiguration("description_package")
+    name = LaunchConfiguration("name")
+    parent = LaunchConfiguration("parent")
+    robot_namespace = LaunchConfiguration("namespace")
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", "robot.urdf.xacro"]
+            ),
+            " ",
+            "name:=",
+            name," ","parent:=",parent,
+            " ",
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        namespace=robot_namespace
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+        namespace=robot_namespace
+    )
+
+    return LaunchDescription(
+        declared_arguments
+        + [
+            joint_state_publisher_node,
+            robot_state_publisher_node,
+        ]
+    )
